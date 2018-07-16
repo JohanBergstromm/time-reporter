481d0d4b466cf6340e2d0776c2c1315d1a2d36cf
Johan Bergström <johans_820@hotmail.com>
2018-07-16 16:00:11 +0200

    added hbs templates

--- public/script/modules/script.js
+++ public/script/modules/script.js
@@ -8,11 +8,13 @@ $(document).ready(function() {
 });
 
 function reportTime() {
-    $('.time-form').submit((e) => {
+    $('.time-form').submit(e => {
         e.preventDefault();
 
         var selectedMonth = $('select#month').val();
-        var savedMonths = $('.time-item').find('.monthDone').val();
+        var savedMonths = $('.time-item')
+            .find('.monthDone')
+            .val();
 
         const fromHour = $('#fh').val();
         const fromMin = $('#fm').val();
@@ -24,133 +26,130 @@ function reportTime() {
 
         if ($('.time-item').length > 0 && selectedMonth !== savedMonths) {
             alert('Ny månad är inte tillgänglig');
-            console.log($('.time-item').length, savedMonths)
+            console.log($('.time-item').length, savedMonths);
             return;
         }
 
         data = {
             from: {
                 hour: fromHour,
-                minute: fromMin
+                minute: fromMin,
             },
             to: {
                 hour: toHour,
-                minute: toMin
+                minute: toMin,
             },
             date: {
                 month: month,
-                day: day
+                day: day,
             },
             add: {
-                comment: comment
+                comment: comment,
             },
-            month: month
-        }
+            month: month,
+        };
 
-        $.post('/times', data).then(function(response) {
-            var template = '';
+        $.post('/times', data)
+            .then(function(response) {
+                let template = Handlebars.templates['time']({ response });
 
-            $('.days .day').each(function(i, el) {
-                if (response.add.comment) {
-                    template +=`
-                    <div class="time-item" data-id="${response._id}">
-                        <div class="date">
-                            <p class="m-0 d-none">${response.date.day}/${response.date.month}</p>
-                        </div>
-                        <div class="time">
-                            <p class="m-0">Från: ${response.from.hour}:${response.from.minute} Till ${response.to.hour}:${response.to.minute}</p>
-                        </div>
-                        <div class="comment">
-                            <p class="m-0">Kommentar: ${response.add.comment}</p>
-                        </div>
-                        <a class="remove-time my-2 btn btn-danger">Ta bort</a>
-                        <input type="hidden" class="fhDone" name="fromHour" value="${response.from.hour}">
-                        <input type="hidden" class="fmDone" name="fromMin" value="${response.from.minute}">
-                        <input type="hidden" class="thDone" name="toHour" value="${response.to.hour}">
-                        <input type="hidden" class="tmDone" name="toMin" value="${response.to.minute}">
-                        <input type="hidden" class="commentDone" name="comment" value="${response.add.comment}">
-                        <input type="hidden" class="monthDone" name="month" value="${response.month}">
-                    </div>`
-                } else {
-                    template +=`
-                    <div class="time-item" data-id="${response._id}">
-                        <div class="date">
-                            <p class="m-0 d-none">${response.date.day}/${response.date.month}</p>
-                        </div>
-                        <div class="time">
-                            <p class="m-0">Från: ${response.from.hour}:${response.from.minute} Till ${response.to.hour}:${response.to.minute}</p>
-                        </div>
-                        <a class="remove-time my-2 btn btn-danger">Ta bort</a>
-                        <input type="hidden" class="fhDone" name="fromHour" value="${response.from.hour}">
-                        <input type="hidden" class="fmDone" name="fromMin" value="${response.from.minute}">
-                        <input type="hidden" class="thDone" name="toHour" value="${response.to.hour}">
-                        <input type="hidden" class="tmDone" name="toMin" value="${response.to.minute}">
-                        <input type="hidden" class="monthDone" name="month" value="${response.month}">
-                    </div>`
-                }
+                $('.days .day').each(function(i, el) {
+                    if (response.date.day == $(el).data('day')) {
+                        $(el).append(template);
+                        if ($(el).hasClass('d-none')) {
+                            var date = $(el)
+                                .find('.date p')
+                                .eq(0)
+                                .text();
 
-                if (response.date.day == $(el).data('day')) {
-                    $(el).append(template);
-                    if ($(el).hasClass('d-none')) {
-                        var date = $(el).find('.date p').eq(0).text();
-
-                        $(el).removeClass('d-none');
-                        $(el).find('#date').html(date);
+                            $(el).removeClass('d-none');
+                            $(el)
+                                .find('#date')
+                                .html(date);
+                        }
                     }
-                }
+                });
+                console.log(response);
+            })
+            .fail(function(err) {
+                console.log(err);
             });
-
-            console.log(response);
-        }).fail(function(err) {
-            console.log(err)
-        });
     });
 }
 
 function saveMonthlyTime() {
-    $('.time-bank').submit((e) => {
+    $('.time-bank').submit(e => {
         e.preventDefault();
 
-        const months = ['Januari', 'Februari', 'Mars', 'April', 'Maj', 'Juni', 'Juli', 'Augusti', 'September', 'Oktober', 'November', 'December'];
+        const months = [
+            'Januari',
+            'Februari',
+            'Mars',
+            'April',
+            'Maj',
+            'Juni',
+            'Juli',
+            'Augusti',
+            'September',
+            'Oktober',
+            'November',
+            'December',
+        ];
         let currentTime = new Date();
         let year = currentTime.getFullYear();
         let month = months[$('.monthDone').val() - 1] + ' - ' + year;
         let monthTimes = [];
-        let dataStorage = { month, monthTimes }
+        let dataStorage = { month, monthTimes };
 
         $('.time-bank .time-item').each((i, el) => {
-            const fromHour = $(el).find('.fhDone').val();
-            const fromMin = $(el).find('.fmDone').val();
-            const toHour = $(el).find('.thDone').val();
-            const toMin = $(el).find('.tmDone').val();
-            const month = $(el).find('.monthDone').val();
-            const day = $(el).find('.dayDone').val();
-            const commentt = $(el).find('.commentDone').val();
+            const fromHour = $(el)
+                .find('.fhDone')
+                .val();
+            const fromMin = $(el)
+                .find('.fmDone')
+                .val();
+            const toHour = $(el)
+                .find('.thDone')
+                .val();
+            const toMin = $(el)
+                .find('.tmDone')
+                .val();
+            const month = $(el)
+                .find('.monthDone')
+                .val();
+            const day = $(el)
+                .find('.dayDone')
+                .val();
+            const commentt = $(el)
+                .find('.commentDone')
+                .val();
 
             let data = {
                 from: {
                     hour: fromHour,
-                    minute: fromMin
+                    minute: fromMin,
                 },
                 to: {
                     hour: toHour,
-                    minute: toMin
+                    minute: toMin,
                 },
                 date: {
                     month: month,
-                    day: day
+                    day: day,
                 },
                 add: {
-                    comment: commentt
+                    comment: commentt,
                 },
-                month: month
-            }
+                month: month,
+            };
 
-            $.post('/time-bank', data).then(function(response) {
-                console.log(response);
-            }).fail(function(err) {
-                console.log(err)
-            });
+            $.post('/time-bank', data)
+                .then(function(response) {
+                    console.log(response);
+                })
+                .fail(function(err) {
+                    console.log(err);
+                });
         });
     });
 }
@@ -159,48 +158,23 @@ function getMonthlyTime() {
     $('.get-monthly-time').submit(function(e) {
         e.preventDefault();
 
-        var template = '';
         var month = $('.month-select').val();
 
         var data = {
-            month: month
-        }
+            month: month,
+        };
 
-        $.post('/get-monthly-time', data).then(function(response) {
-            console.log(response)
-            for (let data of response) {
-                //var data = response[item];
-                if (data.add) {
-                    template += `<div class="time-item" data-id="${data._id}" data-day="${data.date.day}">
-                        <div class="date">
-                            <p class="d-none">${data.date.day}/${data.date.month}</p>
-                        </div>
-                        <div class="time">
-                            <p class="m-0">Från: ${data.from.hour}:${data.from.minute} Till: ${data.to.hour}:${data.to.minute}</p>
-                        </div>
-                        <div class="comment">
-                            <p class="m-0">Kommentar: ${data.add.comment}</p>
-                        </div>
-                        <a class="d-none remove-time btn btn-danger my-2">Ta bort</a>
-                    </div>`;
-                } else {
-                    template += `<div class="time-item" data-id="${data._id}" data-day="${data.date.day}">
-                        <div class="date">
-                            <p class="d-none">${data.date.day}/${data.date.month}</p>
-                        </div>
-                        <div class="time">
-                            <p class="m-0">Från: ${data.from.hour}:${data.from.minute} Till: ${data.to.hour}:${data.to.minute}</p>
-                        </div>
-                        <a class="d-none remove-time btn btn-danger my-2">Ta bort</a>
-                    </div>`;
-                }
-            }
-            $('.data-holder').html(template);
-            sortItems();
-            console.log(template)
-        }).fail((err) => {
-            console.log(err);
-        });
+        $.post('/get-monthly-time', data)
+            .then(function(response) {
+                console.log(response);
+                let template = Handlebars.templates['monthlyTime']({ response });
+                $('.data-holder').html(template);
+                sortItems();
+                console.log(template);
+            })
+            .fail(err => {
+                console.log(err);
+            });
     });
 }
 
@@ -208,31 +182,38 @@ function removeTime() {
     $('.time-report .days, .time-bank').on('click', '.remove-time', function() {
         var timeItem = $(this).closest('.time-item');
         var id = timeItem.data('id');
-        var buttonLength = $(this).closest('.day').find('.time-item').length;
+        var buttonLength = $(this)
+            .closest('.day')
+            .find('.time-item').length;
 
         if ($('.time-report').length) {
-            $.post('/times/' + id).then((response) => {
-                console.log(response);
-                timeItem.remove();
-            }).fail((err) => {
-                console.log(err);
-            });
+            $.post('/times/' + id)
+                .then(response => {
+                    console.log(response);
+                    timeItem.remove();
+                })
+                .fail(err => {
+                    console.log(err);
+                });
         } else {
             $('.time-bank .time-item').each(function(i, el) {
                 var id = $(el).data('id');
 
-                $.post('/time-bank/' + id).then((response) => {
-                    console.log(response);
-                    $('.time-bank .days').remove();
-                }).fail((err) => {
-                    console.log(err);
-                });
-
+                $.post('/time-bank/' + id)
+                    .then(response => {
+                        console.log(response);
+                        $('.time-bank .days').remove();
+                    })
+                    .fail(err => {
+                        console.log(err);
+                    });
             });
         }
 
         if (buttonLength === 1) {
-            $(this).closest('.day').addClass('d-none');
+            $(this)
+                .closest('.day')
+                .addClass('d-none');
         }
     });
 }
@@ -293,12 +274,18 @@ function sortItems() {
         daysValue.push($(el).data('day'));
         setTimeout(() => {
             if ($(el).find('.time-item').length) {
-                var date = $(el).find('.date p').eq(0).text();
+                var date = $(el)
+                    .find('.date p')
+                    .eq(0)
+                    .text();
 
                 $(el).removeClass('d-none');
-                $(el).closest('.day').find('#date').html(date);
+                $(el)
+                    .closest('.day')
+                    .find('#date')
+                    .html(date);
             }
-        })
+        });
     });
 
     $('.time-item').each(function(i, el) {
--- public/script/templates/templates.js
+++ public/script/templates/templates.js
@@ -1,11 +1,37 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['monthlyTime'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+templates['monthlyTime'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var stack1, alias1=container.lambda, alias2=container.escapeExpression;
+
+  return "<div class=\"time-item\" data-id=\""
+    + alias2(alias1((depth0 != null ? depth0._id : depth0), depth0))
+    + "\" data-day=\""
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.date : depth0)) != null ? stack1.day : stack1), depth0))
+    + "\">\n    <div class=\"date\">\n        <p class=\"d-none\">"
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.date : depth0)) != null ? stack1.day : stack1), depth0))
+    + "/"
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.date : depth0)) != null ? stack1.month : stack1), depth0))
+    + "</p>\n    </div>\n    <div class=\"time\">\n        <p class=\"m-0\">Från: "
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.from : depth0)) != null ? stack1.hour : stack1), depth0))
+    + ":"
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.from : depth0)) != null ? stack1.minute : stack1), depth0))
+    + " Till: "
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.to : depth0)) != null ? stack1.hour : stack1), depth0))
+    + ":"
+    + alias2(alias1(((stack1 = (depth0 != null ? depth0.to : depth0)) != null ? stack1.minute : stack1), depth0))
+    + "</p>\n    </div>\n"
+    + ((stack1 = helpers["if"].call(depth0 != null ? depth0 : (container.nullContext || {}),((stack1 = (depth0 != null ? depth0.add : depth0)) != null ? stack1.comment : stack1),{"name":"if","hash":{},"fn":container.program(2, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + "    <a class=\"d-none remove-time btn btn-danger my-2\">Ta bort</a>\n</div>\n";
+},"2":function(container,depth0,helpers,partials,data) {
     var stack1;
 
-  return "<p>"
-    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.date : depth0)) != null ? stack1.month : stack1), depth0))
-    + "</p>";
+  return "    <div class=\"comment\">\n        <p class=\"m-0\">Kommentar: "
+    + container.escapeExpression(container.lambda(((stack1 = (depth0 != null ? depth0.add : depth0)) != null ? stack1.comment : stack1), depth0))
+    + "</p>\n    </div>\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? depth0.response : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
 },"useData":true});
 templates['storedTime'] = template({"1":function(container,depth0,helpers,partials,data) {
     var stack1;
@@ -45,21 +71,29 @@ templates['storedTime'] = template({"1":function(container,depth0,helpers,partia
   return ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? depth0.storages : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
 },"useData":true});
 templates['time'] = template({"1":function(container,depth0,helpers,partials,data) {
-    var stack1;
+    var stack1, alias1=container.lambda, alias2=container.escapeExpression;
 
-  return "    <p>Kommentar: "
-    + container.escapeExpression(container.lambda(((stack1 = ((stack1 = (depth0 != null ? depth0.response : depth0)) != null ? stack1.add : stack1)) != null ? stack1.comment : stack1), depth0))
-    + "</p>\n";
+  return "    <div class=\"comment\">\n        <p class=\"m-0\">Kommentar: "
+    + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.response : depth0)) != null ? stack1.add : stack1)) != null ? stack1.comment : stack1), depth0))
+    + "</p>\n        <input type=\"hidden\" class=\"commentDone\" name=\"comment\" value=\""
+    + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.response : depth0)) != null ? stack1.add : stack1)) != null ? stack1.comment : stack1), depth0))
+    + "\">\n    </div>\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, alias1=container.lambda, alias2=container.escapeExpression;
 
-  return "<div class=\"time\" data-id=\""
+  return "<div class=\"time-item\" data-id=\""
     + alias2(alias1(((stack1 = (depth0 != null ? depth0.response : depth0)) != null ? stack1._id : stack1), depth0))
-    + "\">\n    <p>"
+    + "\" data-day=\""
+    + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.response : depth0)) != null ? stack1.date : stack1)) != null ? stack1.day : stack1), depth0))
+    + "\">\n    <div class=\"date\">\n        <p class=\"d-none\">"
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.response : depth0)) != null ? stack1.date : stack1)) != null ? stack1.day : stack1), depth0))
     + "/"
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.response : depth0)) != null ? stack1.date : stack1)) != null ? stack1.month : stack1), depth0))
-    + "</p>\n    <p>Från: "
+    + "</p>\n        <input type=\"hidden\" class=\"dayDone\" name=\"day\" value=\""
+    + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.response : depth0)) != null ? stack1.date : stack1)) != null ? stack1.day : stack1), depth0))
+    + "\">\n        <input type=\"hidden\" class=\"monthDone\" name=\"month\" value=\""
+    + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.response : depth0)) != null ? stack1.date : stack1)) != null ? stack1.month : stack1), depth0))
+    + "\">\n    </div>\n    <div class=\"time\">\n        <p class=\"m-0\">Från: "
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.response : depth0)) != null ? stack1.from : stack1)) != null ? stack1.hour : stack1), depth0))
     + ":"
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.response : depth0)) != null ? stack1.from : stack1)) != null ? stack1.minute : stack1), depth0))
@@ -67,8 +101,16 @@ templates['time'] = template({"1":function(container,depth0,helpers,partials,dat
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.response : depth0)) != null ? stack1.to : stack1)) != null ? stack1.hour : stack1), depth0))
     + ":"
     + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.response : depth0)) != null ? stack1.to : stack1)) != null ? stack1.minute : stack1), depth0))
-    + "</p>\n"
+    + "</p>\n        <input type=\"hidden\" class=\"fhDone\" name=\"fromHour\" value=\""
+    + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.response : depth0)) != null ? stack1.from : stack1)) != null ? stack1.hour : stack1), depth0))
+    + "\">\n        <input type=\"hidden\" class=\"fmDone\" name=\"fromMin\" value=\""
+    + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.response : depth0)) != null ? stack1.from : stack1)) != null ? stack1.minute : stack1), depth0))
+    + "\">\n        <input type=\"hidden\" class=\"thDone\" name=\"toHour\" value=\""
+    + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.response : depth0)) != null ? stack1.to : stack1)) != null ? stack1.hour : stack1), depth0))
+    + "\">\n        <input type=\"hidden\" class=\"tmDone\" name=\"toMin\" value=\""
+    + alias2(alias1(((stack1 = ((stack1 = (depth0 != null ? depth0.response : depth0)) != null ? stack1.to : stack1)) != null ? stack1.minute : stack1), depth0))
+    + "\">\n    </div>\n"
     + ((stack1 = helpers["if"].call(depth0 != null ? depth0 : (container.nullContext || {}),((stack1 = ((stack1 = (depth0 != null ? depth0.response : depth0)) != null ? stack1.add : stack1)) != null ? stack1.comment : stack1),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "    <span class=\"remove-time\">X</span>\n</div>";
+    + "    <a class=\"remove-time btn btn-danger my-2\">Ta bort</a>\n</div>";
 },"useData":true});
 })();
